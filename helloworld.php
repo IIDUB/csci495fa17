fbab1a6166e4b13c19e8313ef08497bcc1dc7b63
IIDUB <hrwegner@g.coastal.edu>
2017-11-07 10:03:37 -0500

    Update helloworld.php
    Added a print statement.

--- helloworld.php
+++ helloworld.php
@@ -5,6 +5,7 @@ echo "Hello, World!  This is Dr. French\n";
 //Next Student here...
 
 // Hunter Wegner
+echo "Hello World! This is Hunter Wegner\n";
 
 //Andrew Giorgio
 echo "Hello, World!  This is Andrew Giorgio\n";
